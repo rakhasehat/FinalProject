be93286f09804f3f3d760738763f6b33fec14547
M. Rakha Albaihaqi Nasrudin <rakhakok@gmail.com>
2019-04-07 12:29:16 +0700

    First Commit

---
+++ app/src/main/java/example/rakhasehat/com/kantinku/Model/User.java
@@ -0,0 +1,31 @@
+package example.rakhasehat.com.kantinku.Model;
+
+public class User {
+    private String email;
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(String email, String password){
+        email = email;
+        password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
